7cc06e5a5e63597c714f8a354be02ba62364d523
jbmitchell7 <jbmitchell747@gmail.com>
2021-08-09 19:26:07 -0500

    finished exercise 1-2. added array list of pokemon to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-alert('Hello World');
-let favoriteFood = 'Pizza'
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', type: 'Grass', height: 0.7 },
+    { name: 'Ivysaur', type: 'Grass', height: 1 },
+    { name: 'Venusaur', type: 'Grass', height: 2 },
+    { name: 'Charmander', type: 'Fire', height: 0.6 },
+    { name: 'Charmeleon', type: 'Fire', height: 1.1 },
+    { name: 'Charizard', type: 'Fire', height: 1.7 },
+    { name: 'Squirtle', type: 'Water', height: 0.5 },
+    { name: 'Wartortle', type: 'Water', height: 1 },
+    { name: 'Blastoise', type: 'Water', height: 1.6 }
+];
